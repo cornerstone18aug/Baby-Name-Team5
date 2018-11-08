2813bd959b2ccdaa5358869099b20898859c6259
Hao-Tse <maxwuhz@gmail.com>
2018-11-07 16:22:06 -0800

    kind of work

--- src/Main.java
+++ src/Main.java
@@ -4,52 +4,43 @@ public class Main {
 
     public static void main(String[] args) {
 
-        File sourceFile = new File(args[0]);
-        if (!sourceFile.exists()) {
-            System.out.println("Source file " + args[0] + " does not exist.");
-            System.exit(1);
-        }
-
-//        File targetFile = new File(args[1]);
-//        if (targetFile.exists()) {
-//            System.out.println("Target file " + args[1] + " already exists.");
-//        }
-
-        // create an input stream
-        BufferedInputStream input = null;
-//        BufferedOutputStream output = null;
+        FileInputStream input = null;
+        BufferedReader in = null;
+        File sourceFile = new File("babynames/baby1990.html");
         try {
-            input = new BufferedInputStream(new FileInputStream(sourceFile));
-         //   output = new BufferedOutputStream(new FileOutputStream(targetFile));
-//            System.out.println("The file " + args[0] + " has " + input.available() + " bytes.");
+            in = new BufferedReader(new FileReader(sourceFile));
+            String line;
+            while ((line = in.readLine()) != null) { // -1 means end of the File
+                String [] garray = line.split("<td>|</td>");
 
-            // continuously read a byte from input and write it to output.
-            int line;
-           while ((line = input.read()) != -1 ) {
-//               output.write(line);
-               System.out.println(line);
-          }
+                for(int i = 1; i < garray.length; i+=2){
+                    try{
+                        Integer.parseInt(garray[i]);
+                        System.out.println(garray[i]);
+                        System.out.println(garray[i+2]);
+                        System.out.println(garray[i+4]);
+
+                    }catch (Exception e)
+                    {
+                    }
+                }
+            }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (input != null) {
+            if (in != null) {
                 try {
-                    input.close();
+                    in.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-//            if (output != null) {
-//                try {
-//                    output.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
         }
+
     }
+
 
 }
