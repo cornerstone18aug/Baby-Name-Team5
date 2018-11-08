893c61118ffc95805138c723f19f9bcadd11758e
pauloschmidt <paulosergiofuno@gmail.com>
2018-11-08 09:57:56 -0800

    change the line.matches to  Matcher match = Pattern.compile("()").matcher(line);
    The program before was not applying the regex to read the
    file.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
 
 
-        File file1 = new File("babynames/baby1990.html");
+        File file1 = new File("babynames/baby1990.html"); // Will only work with the file baby1990.html
 
 
         extractNames(file1);
@@ -17,6 +19,7 @@ public class Main {
     static void extractNames(File filename)
     {
         BufferedReader in = null;
+        int x = 1;
 
         try
         {
@@ -24,11 +27,20 @@ public class Main {
             String line;
             while((line = in.readLine()) != null)
             {
-                System.out.println(line);
-                if(line.matches("(\\d\\d\\d\\d)"))
-                {
-                    System.out.println(line);
+//                System.out.println(line);
+                Matcher match = Pattern.compile("(<tr align=\"right\"><td>[0-9].*</td><td>[A-Z].*</td><td>[A-Z].*</td>)").matcher(line);
+                // Need to find the regex to make it work
+                while (match.find()) {
+                    System.out.printf("%d. ",x); // we can print the numbers like this, this way we could make the regex to find only the names
+                    System.out.println(match.group());
+                    x++;
                 }
+
+
+//                if(line.matches("[.*]"))
+//                {
+//                    System.out.println(line);
+//                }
             }
         }
         catch(Exception e)
